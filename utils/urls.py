01d289272e3e6e1630178b0b4dce142b2a4a9e77
Nakul93 <nakulnair497@gmail.com>
2021-06-13 10:15:10 +0530

    Android App for OTP retrieving

--- utils/urls.py
+++ utils/urls.py
@@ -1,10 +1,14 @@
 BOOKING_URL = "https://cdn-api.co-vin.in/api/v2/appointment/schedule"
 BENEFICIARIES_URL = "https://cdn-api.co-vin.in/api/v2/appointment/beneficiaries"
-CALENDAR_URL_DISTRICT = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id={0}&date={1}"
-CALENDAR_URL_PINCODE = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByPin?pincode={0}&date={1}"
+CALENDAR_URL_DISTRICT_PRO = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id={0}&date={1}"
+CALENDAR_URL_DISTRICT_PUB = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={0}&date={1}"
+CALENDAR_URL_PINCODE_PRO = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByPin?pincode={0}&date={1}"
+CALENDAR_URL_PINCODE_PUB = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={0}&date={1}"
+FIND_URL_DISTRICT_PRO = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/findByDistrict?district_id={0}&date={1}"
+FIND_URL_DISTRICT_PUB = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={0}&date={1}"
+FIND_URL_PINCODE_PRO = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/findByPin?pincode={0}&date={1}"
+FIND_URL_PINCODE_PUB = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode={0}&date={1}"
 CAPTCHA_URL = "https://cdn-api.co-vin.in/api/v2/auth/getRecaptcha"
-OTP_PUBLIC_URL = "https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP"
-OTP_PRO_URL = "https://cdn-api.co-vin.in/api/v2/auth/generateMobileOTP"
 APPOINTMENT_SLIP_URL = (
     "https://cdn-api.co-vin.in/api/v2/appointment/appointmentslip/download"
 )
