945ea09c5eb28a8ae086fef2062bda6e18aa3d97
Nakul93 <nakulnair497@gmail.com>
2021-06-14 19:26:35 +0530

    Added KVDB option for auto-OTP

--- VacCowin.py
+++ VacCowin.py
@@ -13,7 +13,11 @@ from colorama import Fore, Style, init
 
 from utils.appointment import checkAndBook
 from utils.displayData import displayInfoDict
-from utils.generateOTP import generateTokenOTP
+from utils.generateOTP import (
+generateTokenOTP,
+generate_token_OTP_manual,
+)
+
 from utils.urls import *
 from utils.userInfo import (
     collectUserDetails,
@@ -25,7 +29,7 @@ from utils.userInfo import (
 init(convert=True)
 
 WARNING_BEEP_DURATION = (1000, 2000)
-
+KVDB_BUCKET = os.getenv("KVDB_BUCKET")
 
 try:
     import winsound
@@ -55,10 +59,30 @@ else:
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--token", help="Passing the token directly")
+    parser.add_argument("--mobile", help="Pass mobile directly")
+    parser.add_argument("--kvdb-bucket", help="Pass kvdb.io bucket directly")
+    parser.add_argument("--config", help="Config file name")
+    parser.add_argument(
+        "--no-tty",
+        help="Do not ask any terminal inputs. Proceed with smart choices",
+        action="store_false",
+    )
     args = parser.parse_args()
 
-    filename = "vaccine-booking-details.json"
-    mobile = None
+    if args.config:
+        filename = args.config
+    else:
+        filename = "vaccine-booking-details-"
+
+    if args.mobile:
+        mobile = args.mobile
+    else:
+        mobile = None
+        
+    if args.kvdb_bucket:
+        kvdb_bucket = args.kvdb_bucket
+    else:
+        kvdb_bucket = KVDB_BUCKET
 
     print()
     print(f"{Fore.CYAN}", end="")
@@ -73,51 +97,96 @@ def main():
             "referer": "https://selfregistration.cowin.gov.in/",
         }
 
+        token = None
+        otp_pref = "n"
         if args.token:
             token = args.token
         else:
-            print(f"{Fore.YELLOW}", end="")
-            mobile = input("Enter the Registered Mobile Number: ")
-            print(f"{Fore.RESET}", end="")
-            token = generateTokenOTP(mobile, base_request_header)
+            if mobile is None:
+                mobile = input("Enter the registered mobile number: ")
+            if not args.config:
+                filename = filename + mobile + ".json"
+            if not kvdb_bucket:
+                otp_pref = (
+                    input(
+                        "\nDo you want to enter OTP manually, instead of auto-read? \nRemember selecting n would require some setup described in README (y/n Default n): "
+                    )
+                    if args.no_tty
+                    else "n"
+                )
+                otp_pref = otp_pref if otp_pref else "n"
+                if otp_pref == "n":
+                    kvdb_bucket = (
+                        input(
+                            "Please refer KVDB setup in ReadMe to setup your own KVDB bucket. Please enter your KVDB bucket value here: "
+                        )
+                        if args.no_tty and kvdb_bucket is None
+                        else kvdb_bucket
+                    )
+                    if not kvdb_bucket:
+                        print(
+                            "Sorry, having your private KVDB bucket is mandatory. Please refer ReadMe and create your own private KVBD bucket."
+                        )
+                        sys.exit()
+            if kvdb_bucket:
+                print(
+                    "\n### Note ### Please make sure the URL configured in the IFTTT/Shortcuts app on your phone is: "
+                    + "https://kvdb.io/"
+                    + kvdb_bucket
+                    + "/"
+                    + mobile
+                    + "\n"
+                )
+
+
+            while token is None:
+                if otp_pref == "n":
+                    try:
+                        token = generateTokenOTP(
+                            mobile, base_request_header, kvdb_bucket
+                        )
+                    except Exception as e:
+                        print(str(e))
+                        print("OTP Retrying in 5 seconds")
+                        time.sleep(5)
+                elif otp_pref == "y":
+                    token = generate_token_OTP_manual(mobile, base_request_header)
 
         request_header = copy.deepcopy(base_request_header)
         request_header["Authorization"] = f"Bearer {token}"
 
         if os.path.exists(filename):
-            print(f"{Fore.RESET}", end="")
             print(
                 "\n=================================== Note ===================================\n"
             )
-            print(f"{Fore.GREEN}", end="")
             print(
-                f"Information from a Previous Session already exists in {filename} in this directory."
+                f"Info from perhaps a previous run already exists in {filename} in this directory."
             )
-            print(f"{Fore.CYAN}", end="")
             print(
-                f"IMPORTANT: If you're running this application for the first time then we recommend NOT To USE THE FILE!\n"
+                f"IMPORTANT: If this is your first time running this version of the application, DO NOT USE THE FILE!"
             )
-            print(f"{Fore.YELLOW}", end="")
-            try_file = input(
-                "Would you like to see the details from that file and confirm to proceed? (y/n Default y): "
+            try_file = (
+                input(
+                    "Would you like to see the details and confirm to proceed? (y/n Default y): "
+                )
+                if args.no_tty
+                else "y"
             )
-            print(f"{Fore.RESET}", end="")
             try_file = try_file if try_file else "y"
 
             if try_file == "y":
-                print(f"{Fore.RESET}", end="")
                 collected_details = getSavedUserInfo(filename)
                 print(
                     "\n================================= Info =================================\n"
                 )
                 displayInfoDict(collected_details)
 
-                print(f"{Fore.YELLOW}", end="")
-                file_acceptable = input(
-                    "\nProceed with the above Information? (y/n Default n): "
+                file_acceptable = (
+                    input("\nProceed with above info? (y/n Default y): ")
+                    if args.no_tty
+                    else "y"
                 )
-                print(f"{Fore.RESET}", end="")
-                file_acceptable = file_acceptable if file_acceptable else "n"
+                file_acceptable = file_acceptable if file_acceptable else "y"
 
                 if file_acceptable != "y":
                     collected_details = collectUserDetails(request_header)
@@ -130,72 +199,115 @@ def main():
         else:
             collected_details = collectUserDetails(request_header)
             saveUserInfo(filename, collected_details)
-            confirmAndProceed(collected_details)
+            confirmAndProceed(collected_details, args.no_tty)
+
+        # HACK: Temporary workaround for not supporting reschedule appointments
+        beneficiary_ref_ids = [
+            beneficiary["bref_id"]
+            for beneficiary in collected_details["beneficiary_dtls"]
+        ]
+        beneficiary_dtls = fetch_beneficiaries(request_header)
+        if beneficiary_dtls.status_code == 200:
+            beneficiary_dtls = [
+                beneficiary
+                for beneficiary in beneficiary_dtls.json()["beneficiaries"]
+                if beneficiary["beneficiary_reference_id"] in beneficiary_ref_ids
+            ]
+            active_appointments = []
+            for beneficiary in beneficiary_dtls:
+                expected_appointments = (
+                    1
+                    if beneficiary["vaccination_status"] == "Partially Vaccinated"
+                    else 0
+                )
+                if len(beneficiary["appointments"]) > expected_appointments:
+                    data = beneficiary["appointments"][expected_appointments]
+                    beneficiary_data = {
+                        "name": data["name"],
+                        "state_name": data["state_name"],
+                        "dose": data["dose"],
+                        "date": data["date"],
+                        "slot": data["slot"],
+                    }
+                    active_appointments.append(
+                        {"beneficiary": beneficiary["name"], **beneficiary_data}
+                    )
+
+            if active_appointments:
+                print(
+                    "The following appointments are active! Please cancel them manually first to continue"
+                )
+                display_table(active_appointments)
+                beep(WARNING_BEEP_DURATION[0], WARNING_BEEP_DURATION[1])
+                return
+        else:
+            print(
+                "WARNING: Failed to check if any beneficiary has active appointments. Please cancel before using this script"
+            )
+            if args.no_tty:
+                input("Press any key to continue execution...")
 
         info = SimpleNamespace(**collected_details)
 
-        token_valid = True
-        while token_valid:
+        if info.find_option == 1:
+            disable_re_assignment_feature()
+
+        while True:  # infinite-loop
+            # create new request_header
             request_header = copy.deepcopy(base_request_header)
             request_header["Authorization"] = f"Bearer {token}"
 
             # call function to check and book slots
-            token_valid = checkAndBook(
-                request_header,
-                info.beneficiary_dtls,
-                info.location_dtls,
-                info.find_option,
-                info.search_option,
-                min_slots=info.minimum_slots,
-                ref_freq=info.refresh_freq,
-                auto_book=info.auto_book,
-                start_date=info.start_date,
-                vaccine_type=info.vaccine_type,
-                fee_type=info.fee_type,
-            )
+            try:
+                token_valid = is_token_valid(token)
 
-            # check if token is still valid
-            beneficiaries_list = requests.get(BENEFICIARIES_URL, headers=request_header)
-            if beneficiaries_list.status_code == 200:
-                token_valid = True
+                # token is invalid ?
+                # If yes, generate new one
+                if not token_valid:
+                    print("Token is INVALID.")
+                    token = None
+                    while token is None:
+                        if otp_pref == "n":
+                            try:
+                                token = generateTokenOTP(
+                                    mobile, base_request_header, kvdb_bucket
+                                )
+                            except Exception as e:
+                                print(str(e))
+                                print("OTP Retrying in 5 seconds")
+                                time.sleep(5)
+                        elif otp_pref == "y":
+                            token = generate_token_OTP_manual(
+                                mobile, base_request_header
+                            )
 
-            else:
-                # if token invalid, regenerate OTP and new token
-                beep(WARNING_BEEP_DURATION[0], WARNING_BEEP_DURATION[1])
-                print(f"{Fore.RED}", end="")
-                print("\n\nToken is INVALID!")
-                print(f"{Fore.RESET}", end="")
-                token_valid = False
-
-                print(f"{Fore.YELLOW}", end="")
-                tryOTP = input("Do you want to try for a new Token? (y/n Default y): ")
-                for i in range(10, 0, -1):
-                    print(f"{Fore.RED}", end="")
-                    msg = f"Wait for {i} seconds.."
-                    print(msg, end="\r", flush=True)
-                    print(f"{Fore.RESET}", end="")
-                    sys.stdout.flush()
-                    time.sleep(1)
-                if tryOTP.lower() == "y" or not tryOTP:
-                    if not mobile:
-                        print(f"{Fore.YELLOW}", end="")
-                        mobile = input("Enter the Registered Mobile Number: ")
-                    token = generateTokenOTP(mobile, base_request_header)
-                    token_valid = True
-                else:
-                    print(f"{Fore.RED}", end="")
-                    print("Exiting the Script...")
-                    os.system("pause")
-                    print(f"{Fore.RESET}", end="")
+                check_and_book(
+                    request_header,
+                    info.beneficiary_dtls,
+                    info.location_dtls,
+                    info.pin_code_location_dtls,
+                    info.find_option,
+                    info.search_option,
+                    min_slots=info.minimum_slots,
+                    ref_freq=info.refresh_freq,
+                    # auto_book=info.auto_book,
+                    start_date=info.start_date,
+                    vaccine_type=info.vaccine_type,
+                    fee_type=info.fee_type,
+                    mobile=mobile,
+                    # captcha_automation=info.captcha_automation,
+                    dose_num=get_dose_num(collected_details),
+                )
+            except Exception as e:
+                print(str(e))
+                print("Retryin in 5 seconds")
+                time.sleep(5)
 
     except Exception as e:
-        print(f"{Fore.RED}", end="")
         print(str(e))
-        print("Exiting the Script...")
+        print("Exiting Script")
         os.system("pause")
-        print(f"{Fore.RESET}", end="")
-
-
+        
 if __name__ == "__main__":
     try:
         main()
--- utils/generateOTP.py
+++ utils/generateOTP.py
@@ -1,6 +1,8 @@
 import datetime
 import os
 import sys
+import time
+import re
 from hashlib import sha256
 
 import requests
@@ -12,7 +14,7 @@ from utils.urls import *
 init(convert=True)
 
 WARNING_BEEP_DURATION = (1000, 2000)
-
+SMS_REGEX = r"(?<!\d)\d{6}(?!\d)"
 
 try:
     import winsound
@@ -39,16 +41,102 @@ else:
         winsound.Beep(freq, duration)
 
 
-def generateTokenOTP(mobile, request_header):
+def generateTokenOTP(mobile, request_header, kvdb_bucket):
+    """
+    This function generate OTP and returns a new token
+    """
+    storage_url = "https://kvdb.io/" + kvdb_bucket + "/" + mobile
+
+    txnId = clear_bucket_and_send_OTP(storage_url, mobile, request_header)
+    
+    if txnId is None:
+        return txnId
+
+    time.sleep(10)
+    t_end = time.time() + 60 * 3  # try to read OTP for atmost 3 minutes
+    while time.time() < t_end:
+        response = requests.get(storage_url)
+        if response.status_code == 200:
+            print("OTP SMS is:" + response.text)
+            print("OTP SMS len is:" + str(len(response.text)))
+            OTP = extract_from_regex(response.text, SMS_REGEX)
+            if not OTP:
+                time.sleep(5)
+                continue
+            break
+        else:
+            # Hope it won't 500 a little later
+            print("error fetching OTP API:" + response.text)
+            time.sleep(5)
+
+    if not OTP:
+        return None
+
+    print("Parsed OTP:" + OTP)
+
+    data = {"otp": sha256(str(OTP.strip()).encode("utf-8")).hexdigest(), "txnId": txnId}
+    print(f"Validating OTP..")
+
+    token = requests.post(
+        url="https://cdn-api.co-vin.in/api/v2/auth/validateMobileOtp",
+        json=data,
+        headers=request_header,
+    )
+    if token.status_code == 200:
+        token = token.json()["token"]
+    else:
+        print("Unable to Validate OTP")
+        print(token.text)
+        return None
+
+    print(f"Token Generated: {token}")
+    return token
+
+def clear_bucket_and_send_OTP(storage_url, mobile, request_header):
+    print("clearing OTP bucket: " + storage_url)
+    response = requests.put(storage_url, data={})
+    data = {
+        "mobile": mobile,
+        "secret": "U2FsdGVkX1+z/4Nr9nta+2DrVJSv7KS6VoQUSQ1ZXYDx/CJUkWxFYG6P3iM/VW+6jLQ9RDQVzp/RcZ8kbT41xw==",
+    }
+    print(f"Requesting OTP with mobile number {mobile}..")
+    txnId = requests.post(
+        url="https://cdn-api.co-vin.in/api/v2/auth/generateMobileOTP",
+        json=data,
+        headers=request_header,
+    )
+
+    if txnId.status_code == 200:
+        txnId = txnId.json()["txnId"]
+    else:
+        print("Unable to Create OTP")
+        print(txnId.text)
+        if txnId.status_code == 403 or txnId.status_code == 429:
+            handle_rate_limited()
+        time.sleep(5)  # Saftey net against rate limit
+        txnId = None
+
+    return txnId
+    
+    
+def extract_from_regex(text, pattern):
+    """
+    This function extracts all particular string with help of regex pattern from given text
+    """
+    matches = re.findall(pattern, text, re.MULTILINE)
+    if len(matches) > 0:
+        return matches[0]
+    else:
+        return None
+        
+def generate_token_OTP_manual(mobile, request_header):
     """
     This function generate OTP and returns a new token
     """
 
     if not mobile:
-        print(f"{Fore.RED}", end="")
-        print("Mobile Number cannot be empty. Please Try Again...")
+        print("Mobile number cannot be empty")
         os.system("pause")
-        print(f"{Fore.RESET}", end="")
         sys.exit()
 
     valid_token = False
@@ -61,26 +149,20 @@ def generateTokenOTP(mobile, request_header):
             txnId = requests.post(url=OTP_PRO_URL, json=data, headers=request_header)
 
             if txnId.status_code == 200:
-                print(f"{Fore.CYAN}", end="")
                 print(
-                    f"Successfully Requested OTP for the Mobile Number {mobile} at {datetime.datetime.today()}.."
+                    f"Successfully requested OTP for mobile number {mobile} at {datetime.datetime.today()}.."
                 )
-                print(f"{Fore.RESET}", end="")
                 txnId = txnId.json()["txnId"]
 
-                print(f"{Fore.YELLOW}", end="")
                 OTP = input(
-                    "Enter OTP (If you don't recieve OTP in 2 minutes, Press Enter to Retry): "
+                    "Enter OTP (If this takes more than 2 minutes, press Enter to retry): "
                 )
-                print(f"{Fore.RESET}", end="")
                 if OTP:
                     data = {
                         "otp": sha256(str(OTP).encode("utf-8")).hexdigest(),
                         "txnId": txnId,
                     }
-                    print(f"{Fore.CYAN}", end="")
-                    print(f"Validating OTP. Please Wait...")
-                    print(f"{Fore.RESET}", end="")
+                    print(f"Validating OTP..")
 
                     token = requests.post(
                         url="https://cdn-api.co-vin.in/api/v2/auth/validateMobileOtp",
@@ -89,23 +171,15 @@ def generateTokenOTP(mobile, request_header):
                     )
                     if token.status_code == 200:
                         token = token.json()["token"]
-                        print(f"{Fore.GREEN}", end="")
                         print(f"Token Generated: {token}")
-                        print(f"{Fore.RESET}", end="")
                         valid_token = True
                         return token
 
                     else:
-                        print(f"{Fore.RED}", end="")
-                        print("Unable to Validate OTP...")
+                        print("Unable to Validate OTP")
                         print(f"Response: {token.text}")
-                        print(f"{Fore.RESET}", end="")
 
-                        print(f"{Fore.YELLOW}", end="")
-                        retry = input(
-                            f"Want to Retry with the {mobile} ? (y/n Default y): "
-                        )
-                        print(f"{Fore.RESET}", end="")
+                        retry = input(f"Retry with {mobile} ? (y/n Default y): ")
                         retry = retry if retry else "y"
                         if retry == "y":
                             pass
@@ -113,16 +187,12 @@ def generateTokenOTP(mobile, request_header):
                             sys.exit()
 
             else:
-                print(f"{Fore.RED}", end="")
-                print("Unable to Generate OTP...")
+                print("Unable to Generate OTP")
                 print(txnId.status_code, txnId.text)
                 if txnId.status_code == 403 or txnId.status_code == 429:
-                    handleRateLimited()
-                print(f"{Fore.RESET}", end="")
+                    handle_rate_limited()
 
-                print(f"{Fore.YELLOW}", end="")
-                retry = input(f"Want to Retry with the {mobile} ? (y/n Default y): ")
-                print(f"{Fore.RESET}", end="")
+                retry = input(f"Retry with {mobile} ? (y/n Default y): ")
                 retry = retry if retry else "y"
                 if retry == "y":
                     pass
@@ -130,6 +200,4 @@ def generateTokenOTP(mobile, request_header):
                     sys.exit()
 
         except Exception as e:
-            print(f"{Fore.RED}", end="")
             print(str(e))
-            print(f"{Fore.RESET}", end="")
